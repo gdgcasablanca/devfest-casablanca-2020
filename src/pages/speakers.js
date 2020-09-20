4de9c3ab548393d2550e0289ec4cfac06aba8eb7
Omar Houmz <omhoumz@gmail.com>
2020-09-20 13:19:24 +0100

    chore: add speakers pages

---
+++ src/pages/speakers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout/layout'
+
+export default () => {
+  return (
+    <Layout pageTitle=''>
+      <h1>Speakers page</h1>
+    </Layout>
+  )
+}
