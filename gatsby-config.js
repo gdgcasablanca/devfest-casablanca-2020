e948949d40f628b8e873100d4c4f27572a9c6b77
Omar Houmz <omhoumz@gmail.com>
2020-10-04 18:58:49 +0100

    chore: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Devfest Casablanca 2020`,
-    description: `Devfest Casablanca 2020`,
-    author: `@omarhoumz`,
+    title: 'Devfest Casablanca 2020',
+    description: 'Devfest Casablanca 2020',
+    author: '@gdgcasablanca',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `devfest-casablanca-2020`,
-        short_name: `devfest2020`,
-        start_url: `/`,
-        background_color: `#0005df`,
-        theme_color: `#0005df`,
-        display: `minimal-ui`,
-        icon: `src/images/devfest-icon.png`,
+        name: 'devfest-casablanca-2020',
+        short_name: 'devfest2020',
+        start_url: '/',
+        background_color: '#0005df',
+        theme_color: '#0005df',
+        display: 'minimal-ui',
+        icon: 'src/images/devfest-icon.png',
       },
     },
     {
@@ -35,6 +35,31 @@ module.exports = {
         },
       },
     },
+    'gatsby-plugin-eslint-v2',
+    'gatsby-plugin-prettier-eslint',
+    // {
+    //   resolve: ,
+    //   options: {
+    //     prettier: {
+    //       patterns: [
+    //         // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+    //         '**/*.{css,scss,less}',
+    //         '**/*.{json,json5}',
+    //         '**/*.{graphql}',
+    //         '**/*.{md,mdx}',
+    //         '**/*.{html}',
+    //         '**/*.{yaml,yml}',
+    //       ],
+    //     },
+    //     eslint: {
+    //       patterns: '**/*.{js,jsx,ts,tsx}',
+    //       customOptions: {
+    //         fix: true,
+    //         cache: true,
+    //       },
+    //     },
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
